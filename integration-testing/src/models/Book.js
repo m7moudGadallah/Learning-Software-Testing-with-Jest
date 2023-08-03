34e4c87565f87c162e7a9d382260d639f842a9f7
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-08-03 19:32:14 +0300

    Add Book Model

---
+++ integration-testing/src/models/Book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const BookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    price: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
