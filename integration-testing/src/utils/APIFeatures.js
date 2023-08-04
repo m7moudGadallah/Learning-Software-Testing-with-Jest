af838aeed49b5f2da8b3988310c64ead70b783cf
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-08-04 03:04:02 +0300

    Refactor APIFeatures
    - Make this class to be more specific for parsing and manipulating query
      parameters
    - Add unit-tests for this class

--- integration-testing/src/utils/APIFeatures.js
+++ integration-testing/src/utils/APIFeatures.js
@@ -1,45 +1,26 @@
 /**
- * APIFeatures class for handling advanced querying and filtering of database queries.
- * @class APIFeatures
- * @param {object} reqQuery - The request query object.
- * @param {object} model - The database model.
- * @param {string[]} [excludedFields=[]] - The fields to be excluded from the query.
+ * A utility class for parsing and manipulating query parameters for API features.
  */
 class APIFeatures {
     #reqQuery;
-    #resQuery;
 
     /**
-     * Create an instance of the APIFeatures class.
-     * @constructor
-     * @param {object} reqQuery - The request query object.
-     * @param {object} model - The database model.
-     * @param {string[]} [excludedFields=[]] - The fields to be excluded from the query.
+     * Create an instance of APIFeatures.
+     * @param {object} reqQuery - The request query object containing the query parameters.
+     * @param {string[]} [excludedFields=[]] - An array of field names to be excluded from the query parameters.
      */
-    constructor(reqQuery, model, excludedFields = []) {
+    constructor(reqQuery, excludedFields = []) {
         this.#reqQuery = reqQuery;
         this.excludedFields = excludedFields.length
             ? excludedFields
             : ['page', 'sort', 'limit', 'fields', 'select'];
-        this.#resQuery = model;
     }
 
     /**
-     * Get the current query.
-     * @member {object} query
-     * @readonly
+     * Parse and prepare the filter options from the request query.
+     * @returns {object} The filter options object to be used in a MongoDB query.
      */
-    get query() {
-        return this.#resQuery;
-    }
-
-    /**
-     * Filter the query based on the request query parameters.
-     * @function filter
-     * @param {Array} [populates=[]] - The array of fields to populate in the document.
-     * @returns {APIFeatures} - The updated APIFeatures instance.
-     */
-    filter(populates = []) {
+    parseFilterOptions() {
         try {
             // make a copy from reqQuery and delete excludedFields
             let queryObj = { ...this.#reqQuery };
@@ -70,102 +51,51 @@ class APIFeatures {
             // Convert in values to arrays
             convertValuesToArray('$nin');
 
-            this.#resQuery = this.#resQuery.find(queryObj);
-
-            // populate with passed items
-            populates.forEach((item) => this.#resQuery.populate(item));
-
-            return this;
+            return queryObj;
         } catch (err) {
             throw err;
         }
     }
 
     /**
-     * Sort the query based on the request query parameters or provided fields.
-     * @function sort
-     * @param {string} fields - The fields to sort by. Use the following format: "field1,field2,-field3" (e.g., "id,-name") where a prefix of '-' indicates descending order.
-     * @returns {APIFeatures} - The updated APIFeatures instance.
+     * Parse and prepare the sort options from the request query.
+     * @param {string} [fields=''] - Comma-separated list of fields to sort by.
+     * @returns {string} The sort options string to be used in a MongoDB query.
      */
-    sort(fields = '') {
+    parseSortOptoins(fields = '') {
         try {
+            let sortBy = '_id';
+
             if (this.#reqQuery.sort || fields) {
-                const sortBy = (this.#reqQuery.sort || fields)
-                    .split(',')
-                    .join(' ');
-                this.#resQuery = this.#resQuery.sort(sortBy);
-            } else {
-                this.#resQuery = this.#resQuery.sort('_id');
+                sortBy = (this.#reqQuery.sort || fields).split(',').join(' ');
             }
 
-            return this;
+            return sortBy;
         } catch (err) {
             throw err;
         }
     }
 
     /**
-     * Select the fields to include in the query response.
-     * @function select
-     * @param {string} fields - The fields to include. Use the following format: "field1, field2, -field3" (e.g., "id, name, -password") where a prefix of '-' indicates the field should be excluded from the query result.
-     * @returns {APIFeatures} - The updated APIFeatures instance.
+     * Parse and prepare the select options from the request query.
+     * @param {string} [fields=''] - Comma-separated list of fields to include in the query results.
+     * @returns {string} The select options string to be used in a MongoDB query.
      */
-    select(fields = '') {
+    parseSelectOptions(fields = '') {
         try {
+            let selectOptions = '-__v';
+
             if (this.#reqQuery.fields || this.#reqQuery.select || fields) {
-                const _fields = (
+                selectOptions = (
                     this.#reqQuery.fields ||
                     this.#reqQuery.select ||
                     fields
                 )
                     .split(',')
                     .join(' ');
-                this.#resQuery = this.#resQuery.select(_fields);
-            } else {
-                this.#resQuery = this.#resQuery.select('-__v');
             }
 
-            return this;
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    /**
-     * Paginate the query results.
-     * @function paginate
-     * @param {number} [page=1] - The page number. Default is 1.
-     * @param {number} [limit=100] - The number of items per page. Default is 100.
-     * @param {object} pagination - An object to store pagination data.
-     * @param {number} total - The total number of documents in the entire query (not just the current page).
-     * @returns {APIFeatures} - The updated APIFeatures instance.
-     */
-    paginate(page = 1, limit = 100, pagination, total) {
-        try {
-            const _page = +this.#reqQuery.page || page;
-            const _limit = +this.#reqQuery.limit || limit;
-            const startIndex = (_page - 1) * _limit;
-            const endIndex = _page * _limit;
-            this.#resQuery = this.#resQuery.skip(startIndex).limit(_limit);
-
-            if (pagination && total) {
-                // fill pagination object
-                if (endIndex < total) {
-                    pagination.next = {
-                        page: _page + 1,
-                        pageSize: _limit,
-                    };
-                }
-
-                if (startIndex > 0) {
-                    pagination.prev = {
-                        page: _page - 1,
-                        pageSize: _limit,
-                    };
-                }
-            }
-
-            return this;
+            return selectOptions;
         } catch (err) {
             throw err;
         }
