d3a9cb3a9253a863ce963fb78a30bb91021121fe
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-08-05 03:36:42 +0300

    Add register implementation that pass second test
    - Test that password must be 8 chars or more

--- TDD/register.js
+++ TDD/register.js
@@ -1,7 +1,11 @@
-const register = async (user= {email, password, confirmPassword}) => {
-    const {email, password, confirmPassword} = user;
+const register = async (user = { email, password, confirmPassword }) => {
+    const { email, password, confirmPassword } = user;
 
     if (!email || !password || !confirmPassword) {
+        return 400;
+    }
+
+    if (password.trim().length < 8 || confirmPassword.trim().length < 8) {
         return 400;
     }
 };
